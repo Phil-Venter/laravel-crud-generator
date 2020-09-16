0a306d0107c6c95c1869dec6ca07871ca880ec65
Philip <mrphilventer@gmail.com>
2020-09-16 09:46:27 +0200

    updated index.blade

--- src/Templates/view.index.tpl.php
+++ src/Templates/view.index.tpl.php
@@ -1,50 +1,53 @@
 @extends('[[ custom_master ]]')
 
 @section('content')
-<form action="" method="post" id="delete" style="display: none">
-  {{ method_field('DELETE') }}
-  {{ csrf_field() }}
-  <input type="hidden" id="delete-id" name="id" />
-</form>
 <script>
-  function doDelete(id) {
-    if (confirm('You really want to delete this record?')) {
-      $("#delete-id").val(id);
-      $("#delete").attr("action", `{{ route('[[ route_path ]].index') }}/${id}`);
-      $("#delete").submit();
+  function deleteRecord(id) {
+    if (confirm('Are you sure you want to delete this record?')) {
+      const form = document.createElement(`form`);
+      form.method = `POST`;
+      form.action = `{{ route('countries.index') }}/${id}`;
+      form.style = 'display:none';
+      form.innerHTML += `{{ csrf_field() }}`;
+      form.innerHTML += `{{ method_field('DELETE') }}`;
+      document.querySelector(`body`).append(form);
+      form.submit();
     }
   }
 </script>
+
 <h2 class="page-header">{{ ucfirst('[[ model_plural ]]') }}</h2>
 <a href="{{ route('[[ route_path ]].create') }}" class="btn btn-default pull-right" role="button"><i class="fa fa-plus"></i></a>
+
 <div class="panel panel-default">
   <div class="panel-heading">
     List of {{ ucfirst('[[ model_plural ]]') }}
   </div>
   <div class="panel-body">
-    <div class="">
-      <table class="table table-striped" id="thegrid">
-        <thead>
-          <tr>
-            [[ foreach: columns ]]
-            <th>[[ i.display ]]</th>
-            [[ endforeach ]]
-            <th colspan="3"></th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($[[ model_plural ]] as $[[ model_singular ]])
-          <tr>
-            [[ foreach: columns ]]
-            <td>{{ $[[ model_singular ]]->[[ i.name ]] }}</td>
-            [[ endforeach ]]
-            <td><a href="{{ route('[[ route_path ]].show', [ '[[ model_singular ]]' => $[[ model_singular ]]->id ]) }}" class="btn btn-default"><i class="fa fa-eye"></i></a></td>
-            <td><a href="{{ route('[[ route_path ]].edit', [ '[[ model_singular ]]' => $[[ model_singular ]]->id ]) }}" class="btn btn-info"><i class="fa fa-pencil"></i></a></td>
-            <td><a onclick="return doDelete({{ $[[ model_singular ]]->id }})" class="btn btn-danger"><i class="fa fa-trash"></i></a></td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+    <table class="table table-striped" id="thegrid">
+      <thead>
+        <tr>
+          [[ foreach: columns ]]
+          <th>[[ i.display ]]</th>
+          [[ endforeach ]]
+          <th colspan="3" style="width:150px"></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($[[ model_plural ]] as $[[ model_singular ]])
+        <tr>
+          [[ foreach: columns ]]
+          <td>{{ $[[ model_singular ]]->[[ i.name ]] }}</td>
+          [[ endforeach ]]
+          <td style="width:50px"><a href="{{ route('[[ route_path ]].show', [ '[[ model_singular ]]' => $[[ model_singular ]]->id ]) }}" class="btn btn-default"><i class="fa fa-eye"></i></a></td>
+          <td style="width:50px"><a href="{{ route('[[ route_path ]].edit', [ '[[ model_singular ]]' => $[[ model_singular ]]->id ]) }}" class="btn btn-info"><i class="fa fa-pencil"></i></a></td>
+          <td style="width:50px"><a onclick="return deleteRecord({{ $[[ model_singular ]]->id }})" class="btn btn-danger"><i class="fa fa-trash"></i></a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <div class="center">
+      {{ $[[ model_plural ]]->links() }}
     </div>
   </div>
 </div>
